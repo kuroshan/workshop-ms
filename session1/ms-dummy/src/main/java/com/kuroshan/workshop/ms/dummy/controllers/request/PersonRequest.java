0fa5b8ec69ec566371b035bf1bd14f40b694f151
Americo Sierra <eltrogloditakia@gmail.com>
2021-04-30 16:07:28 -0500

    workshop-ms

--- session1/ms-dummy/src/main/java/com/kuroshan/workshop/ms/dummy/controllers/request/PersonRequest.java
+++ session1/ms-dummy/src/main/java/com/kuroshan/workshop/ms/dummy/controllers/request/PersonRequest.java
@@ -1,5 +1,9 @@
 package com.kuroshan.workshop.ms.dummy.controllers.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
@@ -18,21 +22,24 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 public class PersonRequest {
-	
+
 	@JsonProperty("codigo")
 	private Long id;
 
 	@JsonProperty("first-name")
+	@NotNull(message = "First Name cannot be null")
 	private String firstName;
 
 	@JsonProperty("last-name")
 	private String lastName;
-	
+
+	@Min(value = 18, message = "Age should not be less than 18")
+	@Max(value = 65, message = "Age should not be greater than 65")
 	private int age;
 
 	@JsonProperty("type-document")
 	private String typeDocument;
-	
+
 	@JsonProperty("codigo-document")
 	private String idDocument;
 
