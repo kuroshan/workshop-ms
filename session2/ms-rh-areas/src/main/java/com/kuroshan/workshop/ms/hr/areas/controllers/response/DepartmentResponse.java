45102f47662987a377277d4f99b50962e6334831
Americo Sierra <eltrogloditakia@gmail.com>
2021-05-02 17:58:57 -0500

    workshop-ms

---
+++ session2/ms-rh-areas/src/main/java/com/kuroshan/workshop/ms/hr/areas/controllers/response/DepartmentResponse.java
@@ -0,0 +1,31 @@
+package com.kuroshan.workshop.ms.hr.areas.controllers.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+public class DepartmentResponse {
+
+	private long departmentId;
+	private String departmentName;
+	private LocationResponse location;
+
+	private long locationId;
+	private String streetAddress;
+	private String countryId;
+	private String countryName;
+	private long regionId;
+	private String regionName;
+	
+}
