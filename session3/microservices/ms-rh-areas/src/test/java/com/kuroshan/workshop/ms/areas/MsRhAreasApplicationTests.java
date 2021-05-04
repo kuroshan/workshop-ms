b54c3f979077166433bc716ce4d2507e54d89c6d
Americo Sierra <eltrogloditakia@gmail.com>
2021-05-04 12:45:00 -0500

    ms

--- session3/microservices/ms-rh-areas/src/test/java/com/kuroshan/workshop/ms/areas/MsRhAreasApplicationTests.java
+++ session3/microservices/ms-rh-areas/src/test/java/com/kuroshan/workshop/ms/areas/MsRhAreasApplicationTests.java
@@ -1,13 +1,7 @@
 package com.kuroshan.workshop.ms.areas;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsRhAreasApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
--- session3/microservices/ms-rh-employees/src/test/java/com/kuroshan/workshop/ms/hr/employees/MsRhEmployeesApplicationTests.java
+++ session3/microservices/ms-rh-employees/src/test/java/com/kuroshan/workshop/ms/hr/employees/MsRhEmployeesApplicationTests.java
@@ -1,13 +1,8 @@
 package com.kuroshan.workshop.ms.hr.employees;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsRhEmployeesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
